b618d0f5ba1c515eec2cda5b141a58c9986acfeb
NIKsun <chernik2@gmail.com>
2015-07-12 16:17:54 +0300

    Add getting car list from AutoRu

--- SearchMyCar/Searcher.cs
+++ SearchMyCar/Searcher.cs
@@ -8,25 +8,31 @@ namespace SearchMyCar
 {
     class Searcher
     {
+        public string buildRequest(int price_to)
+        {
+            return null;
+        }
         public string search()
         {
             //Отправляем запрос,где textBox1 - строка с адресом
 
-            System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"https://auto.yandex.ru/offers?price_to=200000&sort_offers=cr_date-desc&in_stock=true&year_from=2008&mark=chevrolet&climate=1&airbag=1");
+            System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"http://auto.ru/cars/chevrolet/lacetti/all/?sort%5Bcreate_date%5D=desc");
             System.Net.WebResponse resp = reqGET.GetResponse();
             System.IO.Stream stream = resp.GetResponseStream();
             System.IO.StreamReader sr = new System.IO.StreamReader(stream, Encoding.UTF8);
             string html = sr.ReadToEnd();
-            var file = File.Open(@"E:\1.html", FileMode.Create);
-            file.Write(Encoding.Unicode.GetBytes(html), 0, html.Length);
-            file.Close();
-            HtmlDocument doc = new HtmlDocument();           
 
-            doc.OptionFixNestedTags = true; //Опционально, если требуется
-
+            HtmlDocument doc = new HtmlDocument();
             doc.Load(new StringReader(html));
-            HtmlNode node = doc.DocumentNode.SelectSingleNode("/html/body/div[2]/div[1]/table/tr/td[1]/div/div/div[2]/div[1]/div/div/table/tr[4]/td[4]/div[2]/text()");   
-            return node.OuterHtml;
+            HtmlNode node = doc.DocumentNode.SelectSingleNode(@"/html/body/div[1]/div[3]/article/div[1]/div[1]/div/div[12]");
+            foreach (var child in node.ChildNodes)
+            {
+                if (child.Attributes.Count == 0)
+                    continue;
+                if (child.Attributes[0].Value == "sales-list-item")
+                    Console.WriteLine(child.InnerText);
+            }
+            return null;
         }
     }
 }
