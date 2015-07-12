ef45b2e69e47e612548d454cbf22cb9d0984c5f4
NIKsun <chernik2@gmail.com>
2015-07-12 14:40:53 +0300

    Create MailSender

--- SearchMyCar/Program.cs
+++ SearchMyCar/Program.cs
@@ -9,6 +9,8 @@ namespace SearchMyCar
     {
         static void Main(string[] args)
         {
+            MailSender ms = new MailSender("chernik2@gmail.com", "smtp.yandex.ru", "chernuhinnv@yandex.ru", "chernik2", 465);
+            ms.SendEmail("Hello world!");
         }
     }
 }
